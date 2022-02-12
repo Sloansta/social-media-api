b9362963c8088a0a875e9d0a5cd4eb78fd1a142a
Sloan <Sloan.boyce@gmail.com>
2022-02-11 22:39:14 -0500

    created and fleshed out controller for user

---
+++ controllers/user-controller.js
@@ -0,0 +1,88 @@
+const { User } = require('../models');
+
+const usrNotFound = 'No user found with this id.'
+
+const userController = {
+    // GET all users
+    getAllUsers(req, res) {
+        User.find({})
+            .popluate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // GET one user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .popluate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: usrNotFound });
+                    return;
+                }
+
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // POST create a user
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err))
+    },
+
+    // PUT update a user 
+
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: usrNotFound });
+                    return;
+                }
+
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // DELETE a user
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: usrNotFound });
+                    return;
+                }
+
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
+    }
+
+};
+
+module.exports = userController;
